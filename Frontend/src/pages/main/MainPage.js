8bcd9360d1c3d3a466baff149eb76648fc5b0ec4
Eung-Seok <rwjflrk1234@gmail.com>
2026-09-17 21:58:00 -0700

    fix: remove BOM from MainPage

--- Frontend/src/pages/main/MainPage.js
+++ Frontend/src/pages/main/MainPage.js
@@ -1,4 +1,4 @@
-﻿import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { businessInquiryApi } from '../../api/BusinessInquiryApi';
