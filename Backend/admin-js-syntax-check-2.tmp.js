031c6d892a7ad26aa1a9ee3f1eacf0ee31b951c4
Eung-Seok <rwjflrk1234@gmail.com>
2026-09-17 21:52:56 -0700

    chore: remove generated backend artifacts

--- Backend/admin-js-syntax-check-2.tmp.js
+++
@@ -1,8 +0,0 @@
-﻿        window.addEventListener('popstate', function() {
-            loadAdminContent(buildAdminAjaxUrlFromLocation(window.location), null, { pushHistory: false });
-        });
-
-        $(document).ready(function() {
-            initializeAdminTheme();
-            loadAdminContent(buildAdminAjaxUrlFromLocation(window.location), null, { pushHistory: false });
-        });
